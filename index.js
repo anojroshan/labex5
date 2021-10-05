383f118d9ed4c95ebc04193e4ce3d77d3eb26f8d
anojroshan <73768440+anojroshan@users.noreply.github.com>
2021-10-05 10:44:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+var http = require('http');
+var fs = require('fs');
+const PORT= process.env.PORT||3000;
+var server = http.createServer(function(req,res){
+    res.writeHead(200,{'Content-type':'text/html'});
+    fs.readFile('./index.html',null,function(error,data){
+        if(error){
+            res.writeHead(404);
+            res.write('File not Found!')
+        }else{
+            res.write(data);
+
+        }
+        res.end();
+
+    })
+
+});
+server.listen(3000);
+console.log("Server running");
